5a35453d22ea1ce7dfe6986ca6c00b76e72426b3
notshawarma <ejcezar.falogme@gmail.com>
2024-04-02 10:30:26 +0800

    email

--- server/utils/emailHTMLTemplate.js
+++ server/utils/emailHTMLTemplate.js
@@ -1,4 +1,3 @@
-
 const passwordResetEmailTemplate = `
 <!DOCTYPE html>
 <html lang="en">
@@ -19,10 +18,28 @@ const passwordResetEmailTemplate = `
 </html>
 `;
 
+const orderItemsTableStyle = `
+    border-collapse: collapse;
+    width: 100%;
+`;
+
+const tableHeaderCellStyle = `
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+    background-color: #f2f2f2;
+`;
+
+const tableCellStyle = `
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+`;
+
 const preparingTemplate = (order) => {
-    const orderItemsTable = `
+  const orderItemsTable = `
         <table style="border-collapse: collapse; width: 100%;">
-            <thead>
+            <thead style="background-color: #f2f2f2;">
                 <tr>
                     <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Image</th>
                     <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Name</th>
@@ -32,40 +49,39 @@ const preparingTemplate = (order) => {
             </thead>
             <tbody>
                 ${order.orderItems
-                    .map(
-                        (item) => `
-                            <tr>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"><img src="${item.image}" alt="${item.name}" style="max-width: 100px;"></td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.name}</td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.price}</td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.quantity}</td>
-                            </tr>
-                        `
-                    )
-                    .join("")}
+                  .map(
+                    (item) => `
+                    <tr>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"><img src="${item.image}" alt="${item.name}" style="max-width: 100px;"></td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.name}</td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">$${item.price}</td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.quantity}</td>
+                    </tr>
+                `
+                  )
+                  .join("")}
             </tbody>
         </table>
     `;
 
-    return `
-        <div style="font-family: Arial, sans-serif; text-align: center;">
-            <h2>Your Order is Being Prepared</h2>
-            <p>Dear Customer,</p>
-            <p>Your order with ID ${order._id} is now being prepared and will be shipped soon.</p>
-            <p>Order Items:</p>
+  return `
+        <div style="max-width: 600px; margin: 0 auto; padding: 20px; text-align: center; background-color: #f9f9f9;">
+        <img src="https://res.cloudinary.com/dbujzuiz0/image/upload/v1711968988/logo_pjd0xm.png" alt="Order Image" style="max-width: 20%; height: auto; margin-top: 10px;">
+            <h2 style="color: #333;">Your Order is Being Prepared</h2>
+            <p style="color: #666;">Dear Customer,</p>
+            <p style="color: #666; ">Your order with ID <strong>${order._id}</strong> is now being prepared and will be shipped soon.</p>
             ${orderItemsTable}
-            <p>Overall Price: $${order.totalAmount}</p>
-            <p>Shipping Address: ${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.country} - ${order.shippingInfo.pinCode}</p>
-            <img src="https://img.freepik.com/free-vector/gradient-quill-pen-logo-with-tagline-template_23-2149813051.jpg" alt="Order Image" style="max-width: 30%; height: auto;">
-            <p>Thank you for shopping with us.</p>
+            <p style="color: #666; font-weight: bold;">Overall Price: $${order.totalAmount}</p>
+            <p style="color: #666; font-weight: bold;">Shipping Address: ${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.country} - ${order.shippingInfo.pinCode}</p>
+            <p style="color: #666;">Thank you for shopping with us.</p>
         </div>
     `;
 };
 
 const shippedTemplate = (order) => {
-    const orderItemsTable = `
+  const orderItemsTable = `
         <table style="border-collapse: collapse; width: 100%;">
-            <thead>
+            <thead style="background-color: #f2f2f2;">
                 <tr>
                     <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Image</th>
                     <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Name</th>
@@ -75,40 +91,39 @@ const shippedTemplate = (order) => {
             </thead>
             <tbody>
                 ${order.orderItems
-                    .map(
-                        (item) => `
-                            <tr>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"><img src="${item.image}" alt="${item.name}" style="max-width: 100px;"></td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.name}</td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.price}</td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.quantity}</td>
-                            </tr>
-                        `
-                    )
-                    .join("")}
+                  .map(
+                    (item) => `
+                    <tr>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"><img src="${item.image}" alt="${item.name}" style="max-width: 100px;"></td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.name}</td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">$${item.price}</td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.quantity}</td>
+                    </tr>
+                `
+                  )
+                  .join("")}
             </tbody>
         </table>
     `;
 
-    return `
-        <div style="font-family: Arial, sans-serif; text-align: center;">
-            <h2>Your Order is Being Shipped</h2>
-            <p>Dear Customer,</p>
-            <p>Your order with ID ${order._id} is now being shipped and will be delivered soon.</p>
-            <p>Order Items:</p>
+  return `
+        <div style="max-width: 600px; margin: 0 auto; padding: 20px; text-align: center; background-color: #f9f9f9;">
+        <img src="https://res.cloudinary.com/dbujzuiz0/image/upload/v1711968988/logo_pjd0xm.png" alt="Order Image" style="max-width: 20%; height: auto; margin-top: 10px;">
+            <h2 style="color: #333;">Your Order is Being Shipped</h2>
+            <p style="color: #666;">Dear Customer,</p>
+            <p style="color: #666;">Your order with ID <strong>${order._id}</strong> is now being shipped and will be delivered soon.</p>
             ${orderItemsTable}
-            <p>Overall Price: $${order.totalAmount}</p>
-            <p>Shipping Address: ${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.country} - ${order.shippingInfo.pinCode}</p>
-            <img src="https://img.freepik.com/free-vector/gradient-quill-pen-logo-with-tagline-template_23-2149813051.jpg" alt="Order Image" style="max-width: 30%; height: auto;">
-            <p>Thank you for shopping with us.</p>
+            <p style="color: #666;">Overall Price: $${order.totalAmount}</p>
+            <p style="color: #666;">Shipping Address: ${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.country} - ${order.shippingInfo.pinCode}</p>
+            <p style="color: #666;">Thank you for shopping with us.</p>
         </div>
     `;
 };
 
 const deliveredTemplate = (order) => {
-    const orderItemsTable = `
+  const orderItemsTable = `
         <table style="border-collapse: collapse; width: 100%;">
-            <thead>
+            <thead style="background-color: #f2f2f2;">
                 <tr>
                     <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Image</th>
                     <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Name</th>
@@ -118,35 +133,38 @@ const deliveredTemplate = (order) => {
             </thead>
             <tbody>
                 ${order.orderItems
-                    .map(
-                        (item) => `
-                            <tr>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"><img src="${item.image}" alt="${item.name}" style="max-width: 100px;"></td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.name}</td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.price}</td>
-                                <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.quantity}</td>
-                            </tr>
-                        `
-                    )
-                    .join("")}
+                  .map(
+                    (item) => `
+                    <tr>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"><img src="${item.image}" alt="${item.name}" style="max-width: 100px;"></td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.name}</td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">$${item.price}</td>
+                        <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${item.quantity}</td>
+                    </tr>
+                `
+                  )
+                  .join("")}
             </tbody>
         </table>
     `;
 
-    return `
-        <div style="font-family: Arial, sans-serif; text-align: center;">
-            <h2>Your Order is Delivered</h2>
-            <p>Dear Customer,</p>
-            <p>Your order with ID ${order._id} has been delivered successfully.</p>
-            <p>Order Items:</p>
+  return `
+        <div style="max-width: 600px; margin: 0 auto; padding: 20px; text-align: center; background-color: #f9f9f9;">
+        <img src="https://res.cloudinary.com/dbujzuiz0/image/upload/v1711968988/logo_pjd0xm.png" alt="Order Image" style="max-width: 20%; height: auto; margin-top: 10px;">
+            <h2 style="color: #333;">Your Order is Delivered</h2>
+            <p style="color: #666;">Dear Customer,</p>
+            <p style="color: #666;">Your order with ID <strong>${order._id}</strong> has been delivered successfully.</p>
             ${orderItemsTable}
-            <p>Overall Price: $${order.totalAmount}</p>
-            <p>Shipping Address: ${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.country} - ${order.shippingInfo.pinCode}</p>
-            <img src="https://img.freepik.com/free-vector/gradient-quill-pen-logo-with-tagline-template_23-2149813051.jpg" alt="Order Image" style="max-width: 30%; height: auto;">
-            <p>Thank you for shopping with us.</p>
+            <p style="color: #666;">Overall Price: $${order.totalAmount}</p>
+            <p style="color: #666;">Shipping Address: ${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.country} - ${order.shippingInfo.pinCode}</p>
+            <p style="color: #666;">Thank you for shopping with us.</p>
         </div>
     `;
 };
 
-
-export { passwordResetEmailTemplate, preparingTemplate, shippedTemplate, deliveredTemplate  };
+export {
+  passwordResetEmailTemplate,
+  preparingTemplate,
+  shippedTemplate,
+  deliveredTemplate,
+};
